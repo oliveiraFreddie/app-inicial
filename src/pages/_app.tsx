470abef2ef44127bade921a59a2b253d50ef1a66
Frederico Oliveira <freddie.oliveira95@gmail.com>
2024-10-06 19:07:38 +0100

    2o_exemplo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
+import { CarrinhoProvider } from '@/data/context/CarrinhoContext'
 import { ContadorProvider } from '@/data/context/ContadorContext'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ContadorProvider>
-      <Component {...pageProps} />
-    </ContadorProvider>
+    <CarrinhoProvider>
+      <ContadorProvider>
+        <Component {...pageProps} />
+      </ContadorProvider>
+    </CarrinhoProvider>
   )
 }
